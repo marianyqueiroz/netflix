616b70ac9f2ca11c33f091d0f0abd0f32b838bc1
marianyqueiroz <marianyqs@gmail.com>
2022-12-17 22:03:24 -0300

    add node, style, js and index

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - javascript
 - nodeJS
+- https://www.themoviedb.org/
---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "9b188b7619a5d583b6f66fea48633922";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
